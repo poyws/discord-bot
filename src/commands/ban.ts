5d4a39c9154729441b0879b745b99aec1a6808d0
root <110252997+poyws@users.noreply.github.com>
2024-11-06 12:51:21 -0300

    Update ban.ts

--- src/commands/ban.ts
+++ src/commands/ban.ts
@@ -3,9 +3,12 @@ import { Command } from '../client';
 export const ban: Command = {
   name: 'ban',
   execute: async (message) => {
+    if (!message.member?.permissions.has('BAN_MEMBERS')) {
+      return message.channel.send('Você não tem permissão para banir usuários.');
+    }
     const member = message.mentions.members?.first();
-    if (!member) return message.channel.send('Please mention a user to ban!');
+    if (!member) return message.channel.send('Por favor, mencione um usuário para banir!');
     await member.ban();
-    message.channel.send(`${member.user.tag} has been banned.`);
+    message.channel.send(`${member.user.tag} foi banido.`);
   },
 };
