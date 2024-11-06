5cf80814499b4916b462a5acefb49864004a6d17
root <110252997+poyws@users.noreply.github.com>
2024-11-06 12:51:36 -0300

    Update kick.ts

--- src/commands/kick.ts
+++ src/commands/kick.ts
@@ -3,9 +3,12 @@ import { Command } from '../client';
 export const kick: Command = {
   name: 'kick',
   execute: async (message) => {
+    if (!message.member?.permissions.has('KICK_MEMBERS')) {
+      return message.channel.send('Você não tem permissão para expulsar usuários.');
+    }
     const member = message.mentions.members?.first();
-    if (!member) return message.channel.send('Please mention a user to kick!');
+    if (!member) return message.channel.send('Por favor, mencione um usuário para expulsar!');
     await member.kick();
-    message.channel.send(`${member.user.tag} has been kicked.`);
+    message.channel.send(`${member.user.tag} foi expulso.`);
   },
 };
